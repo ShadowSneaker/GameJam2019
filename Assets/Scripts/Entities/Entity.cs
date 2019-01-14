3ec59a8f65192a304ea9d1623fe3916e587435fe
ShadowSneaker <danielsfoyle@gmail.com>
2019-01-14 13:18:46 +0000

    Entity

--- Assets/Scripts/Entities/Entity.cs
+++ Assets/Scripts/Entities/Entity.cs
@@ -7,13 +7,23 @@ public class Entity : MonoBehaviour
     public float MaxHealth = 100.0f;
     protected float Health;
 
-    public float MovementSpeed = 10.0f;
+    private float MovementSpeed;
+    public float MaxSpeed = 600.0f;
+
+    public float JumpStrength;
+
+    private bool Dead;
+
+    private Rigidbody Rigid;
 
     
 	// Use this for initialization
 	void Start ()
     {
-		
+        Health = MaxHealth;
+        MovementSpeed = MaxSpeed;
+
+        Rigid = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -26,5 +36,48 @@ public class Entity : MonoBehaviour
     public void TakeDamage()
     {
         Health -= 50.0f;
+
+        if (Health <= 0.0f)
+        {
+            Dead = false;
+
+            // Play Death sound
+        }
+        else
+        {
+            // Play hurt sound
+            
+        }
+
+        float SpeedMultiplier;
+        SpeedMultiplier = Mathf.Clamp(Health, 0.5f, 1);
+        MovementSpeed = MaxSpeed * SpeedMultiplier;
     }
+
+
+    public void Move(Vector3 MoveDir)
+    {
+        Vector3 Vec;
+        Vec = new Vector3(MoveDir.x * MovementSpeed, Rigid.velocity.y, MoveDir.z * MovementSpeed);
+        Rigid.velocity = Vec;
+
+    }
+
+
+    public void Jump()
+    {
+       if (OnGround())
+       {
+            Rigid.AddForce(Vector3.up * JumpStrength);
+       }
+    }
+
+
+    public bool OnGround()
+    {
+        return Physics.Linecast(transform.position, Vector3.down * 2);
+    }
+
+
+
 }
---
+++ Assets/Scripts/Entities/Player/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Entity This;
+
+    private Vector3 MoveDir;
+
+	// Use this for initialization
+	void Start ()
+    {
+        This = GetComponent<Entity>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if (This.OnGround())
+        //{
+            MoveDir = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            MoveDir = transform.TransformDirection(MoveDir);
+
+            if (Input.GetButton("Jump"))
+            {
+                This.Jump();
+            }
+        //}
+
+        This.Move(MoveDir);
+	}
+}
