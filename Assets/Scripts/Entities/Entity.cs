50957a1643f8e458d589ac5d8292e272b64239e3
ShadowSneaker <danielsfoyle@gmail.com>
2019-01-14 12:03:09 +0000

    Project

---
+++ Assets/Scripts/Entities/Entity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public float MaxHealth = 100.0f;
+    protected float Health;
+
+    public float MovementSpeed = 10.0f;
+
+    
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+    public void TakeDamage()
+    {
+        Health -= 50.0f;
+    }
+}
---
+++ Assets/Scripts/Entities/Player/TEMP.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEMP : MonoBehaviour {
+
+    public float Speed = 6.0f;
+    public float JumpStrength = 8.0f;
+    public float Gravity = 20.0f;
+
+    CharacterController Control;
+
+    Vector3 MoveDir;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Control = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Control.isGrounded)
+        {
+            MoveDir = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            MoveDir = transform.TransformDirection(MoveDir);
+            MoveDir *= Speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                MoveDir.y = JumpStrength;
+            }
+        }
+
+        MoveDir.y -= Gravity * Time.deltaTime;
+
+        Control.Move(MoveDir * Time.deltaTime);
+	}
+}
