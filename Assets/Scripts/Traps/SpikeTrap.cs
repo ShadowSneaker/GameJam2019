e09814cf1c5631fb56ec7855586b456fe6c2a899
Byron <byronandrews-salvietto@live.co.uk>
2019-01-14 13:19:21 +0000

    Added traps
    Spike trap aaded and done
    Door trap began

---
+++ Assets/Scripts/Traps/SpikeTrap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrap : MonoBehaviour
+{
+
+
+    // upon collision with the spike trap
+
+    private void OnCollisionEnter(Collision Col)
+    {
+        if(Col.gameObject.CompareTag("Player"))
+        {
+        
+            Col.gameObject.GetComponent<Entity>().TakeDamage();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Traps/WallBlock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallBlock : MonoBehaviour
+{
+
+    public GameObject NodeObject;
+    public GameObject TriggerPoint;
+
+
+    public float CloseSpeed;
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if(other.gameObject.CompareTag("Player"))
+       {
+            float speed = CloseSpeed * Time.deltaTime;
+
+            transform.position = Vector3.MoveTowards(transform.position, NodeObject.transform.position, speed);
+       }
+    }
+
+
+}
