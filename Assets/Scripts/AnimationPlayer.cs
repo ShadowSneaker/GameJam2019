f2039eeb601ee0173c68d7d07739e46267e3a70f
ShadowSneaker <danielsfoyle@gmail.com>
2019-01-15 11:12:53 +0000

    PlayerChanges
    And other stuff

---
+++ Assets/Scripts/AnimationPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPlayer : MonoBehaviour
+{
+    public bool PlayOnStart;
+    public bool Reversed;
+
+    private Animation Anim;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        Anim = GetComponent<Animation>();
+
+        if (Anim)
+        {
+            if (PlayOnStart)
+            {
+                Anim.Play(Anim.clip.name);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
