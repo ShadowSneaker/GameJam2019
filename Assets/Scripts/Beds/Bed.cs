2e105431ffeeffc35d9d8ec55d2c06f363c68c68
Byron <byronandrews-salvietto@live.co.uk>
2019-01-17 11:55:57 +0000

    Respawn and Level
    Respawn Script made with bed script
    And level Finished

---
+++ Assets/Scripts/Beds/Bed.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bed : MonoBehaviour
+{
+    // there will be a function in bed that gets the spawn point transform and sets it to that bed fornow i have it as a trigger collision
+
+        //sets the players new spawnpoint
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<Respawn>().Spawnpoint = transform;
+    }
+
+
+
+}
---
+++ Assets/Scripts/Beds/Respawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+
+    public Transform Spawnpoint;
+    Entity PlayerEntity;
+    //may not be needed
+    bool Respawning;
+
+
+    private void Start()
+    {
+        PlayerEntity = transform.GetComponent<Entity>();
+        Respawning = false;
+    }
+
+    // function to respawn player at that position
+    public void RespawnPlayer()
+    {
+        PlayerEntity.transform.position = Spawnpoint.position;
+    }
+
+}
